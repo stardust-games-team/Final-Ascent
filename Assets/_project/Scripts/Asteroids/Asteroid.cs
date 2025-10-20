0c9e00eea98c7e952fed979fc67e60c6f3656311
CheezusCrust01 <114027826+CheezusCrust01@users.noreply.github.com>
2025-10-20 16:32:26 -0700

    chages files

---
+++ Assets/_project/Scripts/Asteroids/Asteroid.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour, IDamageable
+{
+    // [SerializeField] private FracturedAsteroid _fracturedAsteroidPrefab;
+
+}
+
+public interface IDamageable
+{
+  
+}
