8719440244ee849c432eb22faad581afab228a64
CheezusCrust01 <114027826+CheezusCrust01@users.noreply.github.com>
2025-10-20 17:11:00 -0700

    added files

---
+++ Assets/_project/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool ShouldQuitGame => Input.GetKeyUp(KeyCode.Escape);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldQuitGame)
+        {
+            ShouldGame();
+        }
+    }
+
+    void ShouldGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#else
+        // todo handle WebGL
+        Application.Quit();
+#endif
+    }
+}
